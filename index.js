7a5e7562c027319f31600febcdbd72b04bca460d
bolaxd <102311268+bolaxd@users.noreply.github.com>
2022-05-17 17:11:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ var length = 40; //length of ground
 
 var geometry = new THREE.PlaneGeometry( width, length); //ThreeJS function to create plane geometry
 var texture;
-texture = new THREE.TextureLoader().load( "https://telegra.ph/file/24de8906ade9f03689fd7.jpg" );
+texture = new THREE.TextureLoader().load( "https://www.dropbox.com/s/rro7rfw1i34lmxp/Proyek%20Baru%20161%20%5B19202F3%5D.png?dl=0" );
 var groundmat = new THREE.MeshLambertMaterial({ //Sets color and material attributes for plane
 	color: 0x00c0ff,
 	map:texture, 
